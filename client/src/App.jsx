30b7346d1a8c8d30f6fad7e2ddf0916c58ffa331
san-pok <santoshs.pokhrel@gmail.com>
2024-05-02 09:30:42 +1000

    Create Routes for all the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Signin from "./components/pages/Signin";
+import Signup from "./components/pages/Signup";
+import Dashboard from "./components/pages/Dashboard";
+import Projects from "./components/pages/Projects";
+import Contact from "./components/pages/Contact";
 function App() {
   return (
     <>
-      <h1 className="text-red-500">Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
